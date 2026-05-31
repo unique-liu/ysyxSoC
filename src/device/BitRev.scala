184315de96fb73eeb6443a3edbbed77811becf82
2023k8009929022-LiuYaoqing <liuyaoqing23@mails.ucas.ac.cn>
2026-05-31 16:13:17 +0800

    B2: finish bitrev and xip

--- src/device/BitRev.scala
+++ src/device/BitRev.scala
@@ -9,5 +9,9 @@ class bitrev extends BlackBox {
 
 class bitrevChisel extends RawModule { // we do not need clock and reset
   val io = IO(Flipped(new SPIIO(1)))
-  io.miso := true.B
+  val reg =withClockAndReset((~io.sck).asClock,io.ss.asBool.asAsyncReset) { RegInit(0.U(8.W)) }
+  val counter =withClockAndReset((~io.sck).asClock,io.ss.asBool.asAsyncReset) { RegInit(0.U(4.W)) }
+  counter := Mux(~counter(3), counter + 1.U, counter)
+  reg := Mux(~counter(3),Cat(reg(6,0), io.mosi), Cat(0.U(1.W),reg(7,1))) // shift in the bits on the rising edge of sck
+  io.miso := Mux(io.ss.asBool ,1.U, reg(0) & counter(3))
 }
