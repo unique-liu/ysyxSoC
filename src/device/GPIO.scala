233c46b294e7b1b749c01a8be15d012e11a7f548
2023k8009929022-LiuYaoqing <liuyaoqing23@mails.ucas.ac.cn>
2026-06-07 11:10:18 +0800

    B2: gpio finished

--- src/device/GPIO.scala
+++ src/device/GPIO.scala
@@ -26,7 +26,94 @@ class gpio_top_apb extends BlackBox {
 }
 
 class gpioChisel extends Module {
+  def hex2seg(hex: UInt): UInt = {
+    // 0-9, A-F
+    //assume from high to low: A, B, C, D, E, F, G, DP,  low active
+    val segs = VecInit(
+      "b00000011".U, // 0
+      "b10011111".U, // 1
+      "b00100101".U, // 2
+      "b00001101".U, // 3//not fixed
+      "b10011001".U, // 4//not fixed
+      "b01001001".U, // 5//not fixed
+      "b01000001".U, // 6//not fixed
+      "b00011111".U, // 7//not fixed
+      "b00000001".U, // 8//not fixed
+      "b00001001".U, // 9//not fixed
+      "b00010001".U, // A//not fixed
+      "b11000001".U, // B//not fixed
+      "b01100011".U, // C//not fixed
+      "b10000101".U, // D//not fixed
+      "b01100001".U, // E//not fixed
+      "b01110001".U  // F//not fixed
+
+      //debug
+      // "b00000001".U,
+      // "b00000010".U,
+      // "b00000100".U,
+      // "b00001000".U,
+      // "b00010000".U,
+      // "b00100000".U,
+      // "b01000000".U,
+      // "b10000000".U,
+    )
+    segs(hex)
+  }
   val io = IO(new GPIOCtrlIO)
+  val led_out = RegInit(0.U(16.W))
+  val seg_hex = RegInit("h01234567".U(32.W))
+  val seg_out = Wire(Vec(8, UInt(8.W)))
+  val rdata   = Wire(UInt(32.W))
+
+  for (i <- 0 until 8) {
+    seg_out(i) := hex2seg(seg_hex(4*i+3, 4*i))
+  }
+
+  io.gpio.out := led_out
+  io.gpio.seg := seg_out
+  
+
+  val idle :: read :: write :: Nil = Enum(3)
+  val state = RegInit(idle)
+  io.in.pready := false.B
+  io.in.prdata := 0.U(32.W)
+  io.in.pslverr := false.B
+  switch(state) {
+    is(idle) {
+      when(io.in.psel && io.in.penable) {
+        state := Mux(io.in.pwrite, write, read)
+      }
+    }
+    is(read) {
+      state := idle
+      io.in.pready := true.B
+      io.in.prdata := rdata
+    }
+    is(write) {
+      state := idle
+      io.in.pready := true.B
+    }
+  }
+
+  rdata := 0.U(32.W)
+  switch(io.in.paddr(3,0)){
+    is(0.U) { // LED
+      when(state === write) {
+        led_out := io.in.pwdata(15, 0)
+      }
+      rdata := Cat(0.U(16.W), led_out)
+    }
+    is(4.U) { // SEG
+      when(state === write) {
+        seg_hex := io.in.pwdata
+      }
+      rdata := seg_hex
+    }
+    is(8.U) { // IN
+      rdata := Cat(0.U(16.W), io.gpio.in)
+    }
+  }
+
 }
 
 class APBGPIO(address: Seq[AddressSet])(implicit p: Parameters) extends LazyModule {
@@ -43,7 +130,7 @@ class APBGPIO(address: Seq[AddressSet])(implicit p: Parameters) extends LazyModu
     val (in, _) = node.in(0)
     val gpio_bundle = IO(new GPIOIO)
 
-    val mgpio = Module(new gpio_top_apb)
+    val mgpio = Module(new gpioChisel)
     mgpio.io.clock := clock
     mgpio.io.reset := reset
     mgpio.io.in <> in
